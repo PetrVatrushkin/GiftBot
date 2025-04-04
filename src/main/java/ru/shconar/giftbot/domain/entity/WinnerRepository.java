0be9a0e31cdac4a3b1d28d6f5cc6dbf20188d07d
dmitriykosolobov <kosolobov.2003@gmail.com>
2025-04-04 16:38:21 +0300

    fix transactional

--- src/main/java/ru/shconar/giftbot/domain/entity/WinnerRepository.java
+++ src/main/java/ru/shconar/giftbot/domain/entity/WinnerRepository.java
@@ -1,9 +1,12 @@
 package ru.shconar.giftbot.domain.entity;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WinnerRepository extends JpaRepository<Winner, Long> {
     List<Winner> findByRaffle(Raffle raffle);
+
+    @Transactional
     void deleteByRaffle(Raffle raffle);
 }
